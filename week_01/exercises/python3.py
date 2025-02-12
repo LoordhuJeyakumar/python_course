3ce7fa807f73e991655f1b559b99c4e3b40df631
ponprabham <prabhamurugan2323@gmail.com>
2025-02-12 12:30:45 +0530

    Day 3 Task
    completed

---
+++ week_01/exercises/python3.py
@@ -0,0 +1,31 @@
+# fname=input("Enter the Fname")
+# Sname=input("Enter the Sname")
+# print(f"Hello, {fname} {Sname}! Welcome!")
+
+# string = input("Enter a string: ")
+# print(f"Length of the string: {len(string)}")
+
+# sentence = input("Enter a sentence: ")
+# word = len(sentence.split())
+# print(f"Number of words in sentence: {word}")
+
+# 5th
+# string = input("Enter a string: ")
+# reversed_string = string[::-1]
+# print(f"Reversed string: {reversed_string}")
+
+
+
+
+# username = input("Enter your email: ")
+
+# valid_at = username.find("@")
+# valid_dot = username.find(".")
+
+
+# if valid_at == -1 or valid_dot == -1 :
+#     print("inValid email")
+# else:
+#     print("valid email")
+
+
