0ecda766b2d796514fbaaeeac5268f93904e1936
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-03-27 12:33:50 +0530

    Update homepage view and add about and blog views

--- django/myfirstproject/myfirstproject/views.py
+++ django/myfirstproject/myfirstproject/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def homepage(request):
+
+    
+
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
     return render(request, 'blog.html')
