992dfc18ff3f16717a5e8c0ad0274a5394c501c1
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-03-27 12:32:46 +0530

    Add new URL patterns for post listing, post detail, and feedback submission in blog app

--- django/myfirstproject/blog/urls.py
+++ django/myfirstproject/blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.hello_blog, name='hello_blog'),
+    path('posts/', views.post_list, name='post_list'),
+    path('posts/<int:post_id>/', views.get_post_detail, name='post_detail'),
+    path('feedback/', views.submit_feedback, name='submit_feedback')
 
 ]
